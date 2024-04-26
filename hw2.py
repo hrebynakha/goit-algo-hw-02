c5bb520e3d4164d0ddf38b1cc0b61fa73281429d
Hrebynakha Anatolii <hrebynakha@gmail.com>
2024-04-27 01:51:19 +0300

    add hw2

--- hw2.py
+++ hw2.py
@@ -7,4 +7,24 @@
 """
 from collections import deque
 
-def 
+def check_palindrom(text, ):
+    """Check text palindrom using deque module"""
+    d = deque()
+    for char in text.lower().replace(' ',''):
+        d.append(char)
+    is_palindrom = True
+    while is_palindrom and d:
+        right = d.pop()
+        left = d.popleft() if d else right
+        if right != left:
+            is_palindrom = False
+    return is_palindrom
+
+
+PALINDROM_1 = 'level'
+PALINDROM_2  = 'We panic in a pew'
+NOT_PALINDROM = 'notplindromon'
+
+print(f"Word: '{PALINDROM_1}' is palindrom {check_palindrom(PALINDROM_1)}")
+print(f"Word: '{PALINDROM_2}' is palindrom {check_palindrom(PALINDROM_2)}")
+print(f"Word: '{NOT_PALINDROM}' is palindrom {check_palindrom(NOT_PALINDROM)}")
